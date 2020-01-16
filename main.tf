a8e8950fafcc9e367bb925c2d25219a926ac33ba
Christopher Markieta <markietachristopher@gmail.com>
2020-01-16 01:33:47 -0500

    Moving provider details into resource.

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
-provider "demo" {
+resource "demo_repo_content" "main" {
   organization = "Markieta-Inc"
-}
+  repo = "repo1"
+  branch = "master"
+  file_path = "README.md"
+  file_content = <<EOF
+# Repository Numba 1
 
-resource "demo_repo_content" "main" {}
+Chris, we really need to be more descriptive with these docs...
+EOF
+}
--- provider.go
+++ provider.go
@@ -6,21 +6,6 @@ import (
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
-		Schema: map[string]*schema.Schema{
-			"token": &schema.Schema{
-				Type:        schema.TypeString,
-				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("GITHUB_TOKEN", nil),
-				Description: "The OAuth token used to connect to GitHub.",
-			},
-			"organization": {
-				Type:        schema.TypeString,
-				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("GITHUB_ORGANIZATION", nil),
-				Description: "The GitHub organization name to manage.",
-			},
-		},
-
 		ResourcesMap: map[string]*schema.Resource{
 			"demo_repo_content": resourceRepoContent(),
 		},
